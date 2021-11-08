4a73d95dd8a1a915780ed63f49dbde8ec5d49905
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-08 10:34:09 -0800

    querySelector and querySelectorAll examples

---
+++ README.md
@@ -0,0 +1,10 @@
+# __Replacements for jQuery__
+
+## __Description__
+
+_After learning JavaScript using jQuery to start, I am now learning to write JavaScript with the newer ES6 updates that include a lot of the jQuery features._
+
+### __NOTES__
+
+- NODES, all the elements made in the website, nested inside each other
+
--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-document.body.innerHTML = "scripts"
+//document.body.innerHTML = "scripts"
+
+//Making changes to the DOM
+
+//getElementById
+document.getElementById("title-id").innerHTML = "changed by id";
+
+document.getElementsByClassName("potato-class");
+
+//getElementsByClassName();
+
+//getElementsByTagName();
+
+
+// document.querySelector()
+// let banner = document.querySelector(".title-class");
+// banner.innerHTML = "changed";
+
+
