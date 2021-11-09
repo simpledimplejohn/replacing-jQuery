34a6e7ec0ff8c3501ee96eb89e9cc573db1b870c
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-09 08:47:34 -0800

    finished, pass a name through the functions

--- form.js
+++ form.js
@@ -1,13 +1,15 @@
 let btn = document.querySelector(".test-btn");
 
 
-function firstFunction(e) {
+function firstFunction(e, name) {
   e.preventDefault();
-  btn.innerHTML = "YAY!";
-}
-function secondFunction() {
-  btn.style.backgroundColor = "red";
+  btn.innerHTML = name;
 }
 
-btn.addEventListener("click", firstFunction); //event listener, listen for click, and do?
-btn.addEventListener("click", secondFunction);
+
+btn.addEventListener("click", (e) => { //need to pass the event through from the fx
+  firstFunction(e, "john");
+}); //event listener, listen for click, and do?
+btn.addEventListener("click", () => {    //put a function in an event listener
+  btn.style.backgroundColor = 'orange';
+});
